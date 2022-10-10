a71f682c758303bc462eced3e74dc7e2717a88d3
lunarkbot <58397321+lunarkbot@users.noreply.github.com>
2022-10-10 18:23:42 +0300

    fix: change cookie config

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -135,7 +135,6 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: 'none',
           secure: true,
-          domain: 'localhost',
           path: '/',
         })
         .send({
