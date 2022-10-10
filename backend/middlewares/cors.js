ef791d313a645cf0467a9050d62e264b580aed60
lunarkbot <58397321+lunarkbot@users.noreply.github.com>
2022-10-10 17:02:03 +0300

    fix: change cors rules

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   if (method === 'OPTIONS') {
