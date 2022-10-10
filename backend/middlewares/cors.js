07d4ae4f5b5426a5e83a4cad15483a10baa70a26
lunarkbot <58397321+lunarkbot@users.noreply.github.com>
2022-10-10 16:37:26 +0300

    fix: change access-control-allow

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,9 +7,9 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  //if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', "*");
+  //}
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
