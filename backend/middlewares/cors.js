bd7e242af128c5a5e170578de0312e2b41e3e664
lunarkbot <58397321+lunarkbot@users.noreply.github.com>
2022-10-10 17:07:58 +0300

    fix: add allow credentials

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,6 +9,7 @@ module.exports = (req, res, next) => {
 
   //if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', '*');
   //}
 
   if (method === 'OPTIONS') {
